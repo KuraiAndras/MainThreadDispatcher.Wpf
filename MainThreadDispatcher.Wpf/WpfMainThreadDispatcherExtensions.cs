529114d75bc3f03e6b245b588a3397af70febc8f
Kurai András <k.andras9642@gmail.com>
2020-03-20 19:05:09 +0100

    Add extension

---
+++ MainThreadDispatcher.Wpf/WpfMainThreadDispatcherExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace MainThreadDispatcher.Wpf
+{
+    public static class WpfMainThreadDispatcherExtensions
+    {
+        private static bool _initialized = false;
+        private static IMainThreadDispatcher _dispatcher;
+
+        public static IMainThreadDispatcher Instance
+        {
+            get
+            {
+                if (!_initialized)
+                {
+                    _initialized = true;
+                    _dispatcher = new WpfMainThreadDispatcher();
+                }
+
+                return _dispatcher;
+            }
+        }
+    }
+}
